ca26b54d1e74ecf9a9b2ee823721729db6d56af3
winter223 <link5658@gmail.com>
2021-09-26 11:43:44 +0900

    [#10] 싱글 액티비티 세팅

--- app/src/main/java/com/mashup/kkyuni/MainActivity.kt
+++ app/src/main/java/com/mashup/kkyuni/MainActivity.kt
@@ -2,7 +2,9 @@ package com.mashup.kkyuni
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/mashup/kkyuni/StartFragment.kt
@@ -0,0 +1,14 @@
+package com.mashup.kkyuni
+
+import androidx.fragment.app.Fragment
+
+/**
+ * 실제로는 사용하지 않는 Fragment.
+ * 화면 플로우에 맞게 순서대로 개발되지 않기 때문에
+ * 해당 Fragment 뒤에 각자 개발하는 Fragment 를 붙여 테스트 합니다.
+ * Commit 할 땐 해당 Fragment 를 제외합니다.
+ * 만약 직전 Fragment 가 완성되어 merge 된 상태라면 플로우에 맞게 연결 후 push 합니다.
+ *
+ * Todo: 개발이 완료되거나 더 이상 불필요하다고 판단되는 시점에서 해당 Fragment 를 완전히 제거합니다.
+ */
+class StartFragment: Fragment()
