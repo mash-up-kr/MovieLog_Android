045be2fbd1609d51a2a85e7c034d6106d4c8c9ca
lagojin <jinho6187@gmail.com>
2021-10-02 14:30:37 +0900

    #28 setting presentation 구현

---
+++ feature/setting/presentation/src/main/java/com/mashup/kkyuni/feature/setting/presentation/SettingFragment.kt
@@ -0,0 +1,23 @@
+package com.mashup.kkyuni.feature.setting.presentation
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.mashup.kkyuni.core.BindingFragment
+import com.mashup.kkyuni.feature.setting.presentation.databinding.FragmentSettingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SettingFragment : BindingFragment<FragmentSettingBinding>(R.layout.fragment_setting) {
+
+    private val viewModel: SettingViewModel by viewModels()
+    private val adapter by lazy { SettingAdapter(viewModel) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            recyclerView.adapter = this@SettingFragment.adapter
+        }
+    }
+}
