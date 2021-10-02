9ae001708a124c2c2fd43752ad309c0adcf84642
koba <koba1mobile@gmail.com>
2021-10-03 02:49:24 +0900

    [#32] 데이터모듈 수정

--- feature/playlist/data/src/main/java/com/mashup/kkyuni/playlist/data/PlayListDataModule.kt
+++ feature/playlist/data/src/main/java/com/mashup/kkyuni/playlist/data/PlayListDataModule.kt
@@ -2,16 +2,30 @@ package com.mashup.kkyuni.playlist.data
 
 import com.mashup.kkyuni.playlist.data.repository.PlayListRepository
 import com.mashup.kkyuni.playlist.data.repository.impl.PlayListRepositoryImpl
+import com.mashup.kkyuni.playlist.data.service.PlayListService
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Named
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class PlayListDataModule {
-    @Binds
-    abstract fun bindPlayListRepository(
-        repositoryImpl: PlayListRepositoryImpl
-    ): PlayListRepository
+class PlayListDataModule {
+    @Provides
+    @Singleton
+    fun provideVideoService(@Named("kkyuni_api") retrofit: Retrofit) =
+        retrofit.create(PlayListService::class.java)
+
+    @Module
+    @InstallIn(SingletonComponent::class)
+    interface AbstractModule {
+        @Binds
+        fun bindPlayListRepository(
+            repositoryImpl: PlayListRepositoryImpl
+        ): PlayListRepository
+    }
 }
