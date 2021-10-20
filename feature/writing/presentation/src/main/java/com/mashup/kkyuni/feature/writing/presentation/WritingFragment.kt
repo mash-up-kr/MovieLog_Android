b46809a2b4029abba95b56e8469675db10242b44
koba <koba1mobile@gmail.com>
2021-10-21 01:43:15 +0900

    [#43] presentation 모듈 구성 및 감정 선택 뷰 제작

--- feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/WritingFragment.kt
+++ feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/WritingFragment.kt
@@ -1,4 +1,11 @@
 package com.mashup.kkyuni.feature.writing.presentation
 
-class WritingFragment {
+import androidx.fragment.app.viewModels
+import com.mashup.kkyuni.core.BindingFragment
+import com.mashup.kkyuni.feature.writing.presentation.databinding.FragmentWritingBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WritingFragment: BindingFragment<FragmentWritingBinding>(R.layout.fragment_writing) {
+    private val writingViewModel by viewModels<WritingViewModel>()
 }
---
+++ feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/WritingViewModel.kt
@@ -0,0 +1,10 @@
+package com.mashup.kkyuni.feature.writing.presentation
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class WritingViewModel @Inject constructor(): ViewModel() {
+
+}
