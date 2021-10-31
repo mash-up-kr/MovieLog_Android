14967c234b73a0243abe86b3032a3d29f12c503b
koba <koba1mobile@gmail.com>
2021-10-31 21:14:39 +0900

    [#43] 음악선택 화면으로 이동

--- feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/emotion/WritingEmotionFragment.kt
+++ feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/emotion/WritingEmotionFragment.kt
@@ -3,11 +3,16 @@ package com.mashup.kkyuni.feature.writing.presentation.emotion
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.mashup.kkyuni.core.BindingFragment
 import com.mashup.kkyuni.feature.writing.presentation.R
 import com.mashup.kkyuni.feature.writing.presentation.WritingViewModel
 import com.mashup.kkyuni.feature.writing.presentation.databinding.FragmentWritingEmotionBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class WritingEmotionFragment: BindingFragment<FragmentWritingEmotionBinding>(R.layout.fragment_writing_emotion) {
@@ -18,5 +23,17 @@ class WritingEmotionFragment: BindingFragment<FragmentWritingEmotionBinding>(R.l
         super.onViewCreated(view, savedInstanceState)
 
         binding.emotionViewModel = emotionViewModel
+
+        collectFlows()
+    }
+
+    private fun collectFlows() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                emotionViewModel.nextEvent.collect {
+                    writingViewModel.updateEmotion(it)
+                }
+            }
+        }
     }
 }
---
+++ feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/music/WritingMusicFragment.kt
@@ -0,0 +1,11 @@
+package com.mashup.kkyuni.feature.writing.presentation.music
+
+import com.mashup.kkyuni.core.BindingFragment
+import com.mashup.kkyuni.feature.writing.presentation.R
+import com.mashup.kkyuni.feature.writing.presentation.databinding.FragmentWritingMusicBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WritingMusicFragment: BindingFragment<FragmentWritingMusicBinding>(R.layout.fragment_writing_music) {
+
+}
