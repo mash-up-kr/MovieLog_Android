95fb577b9c542740757f89735156da48fd5908c9
koba <koba1mobile@gmail.com>
2021-10-31 21:50:19 +0900

    [#43] 음악 선택 화면 추가

--- feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/music/WritingMusicFragment.kt
+++ feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/music/WritingMusicFragment.kt
@@ -1,11 +1,57 @@
 package com.mashup.kkyuni.feature.writing.presentation.music
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.mashup.kkyuni.core.BindingFragment
 import com.mashup.kkyuni.feature.writing.presentation.R
+import com.mashup.kkyuni.feature.writing.presentation.WritingViewModel
 import com.mashup.kkyuni.feature.writing.presentation.databinding.FragmentWritingMusicBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class WritingMusicFragment: BindingFragment<FragmentWritingMusicBinding>(R.layout.fragment_writing_music) {
+    private val musicViewModel by viewModels<WritingMusicViewModel>()
+    private val writingViewModel by viewModels<WritingViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+
+        collectFlows()
+    }
+
+    private fun initView() {
+
+    }
+
+    private fun collectFlows() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                musicViewModel.run {
+                    backEvent.collect {
+                        onBackPressed()
+                    }
+
+                    searchEvent.collect {
+                        findNavController().navigate(R.id.musicFragment)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun onBackPressed(){
+        findNavController().popBackStack()
+    }
+
+
 
 }
