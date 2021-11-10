a6f292c872bfb56e229ebe9953d03f0c5ecbe452
koba <koba1mobile@gmail.com>
2021-11-10 20:21:37 +0900

    [#43] data layer추가 및 업로드 플로우 작성

---
+++ feature/writing/data/src/main/java/com/mashup/kkyuni/feature/writing/data/repository/impl/WritingRepositoryImpl.kt
@@ -0,0 +1,35 @@
+package com.mashup.kkyuni.feature.writing.data.repository.impl
+
+import com.mashup.kkyuni.feature.writing.data.repository.WritingRepository
+import com.mashup.kkyuni.feature.writing.data.service.WritingService
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class WritingRepositoryImpl @Inject constructor(
+    private val service: WritingService
+) : WritingRepository {
+
+    override fun createDiary(
+        content: String,
+        diaryType: String,
+        emotion: String,
+        musicPlayTime: Int,
+        musicThumbnailImage: String,
+        musicTitle: String,
+        title: String,
+        youtubeLink: String
+    ) = flow {
+        emit(
+            service.createDiary(
+                content,
+                diaryType,
+                emotion,
+                musicPlayTime,
+                musicThumbnailImage,
+                musicTitle,
+                title,
+                youtubeLink
+            )
+        )
+    }
+}
