df36a5408ddfa27d9e29776179214c27c84d6d6c
koba <koba1mobile@gmail.com>
2021-11-12 18:08:40 +0900

    [#43] 키보드 위로 버튼 올라오도록 수정

--- feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/WritingFragment.kt
+++ feature/writing/presentation/src/main/java/com/mashup/kkyuni/feature/writing/presentation/WritingFragment.kt
@@ -2,6 +2,7 @@ package com.mashup.kkyuni.feature.writing.presentation
 
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.mashup.kkyuni.core.BindingFragment
@@ -12,6 +13,11 @@ import dagger.hilt.android.AndroidEntryPoint
 class WritingFragment: BindingFragment<FragmentWritingBinding>(R.layout.fragment_writing) {
     private val writingViewModel by viewModels<WritingViewModel>()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        applyInputAdjustResize()
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         navigateToEmotionFragment()
@@ -19,5 +25,18 @@ class WritingFragment: BindingFragment<FragmentWritingBinding>(R.layout.fragment
 
     private fun navigateToEmotionFragment() {
         findNavController().navigate(R.id.action_to_writingEmotionFragment)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        clearInputAdjust()
+    }
+
+    private fun applyInputAdjustResize(){
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+    }
+
+    private fun clearInputAdjust(){
+        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_UNSPECIFIED)
     }
 }
