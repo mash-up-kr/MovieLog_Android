ec39ae4f8fb7be4ffb569fb23c3742c650d88d54
Huijiny <jiin20803@gmail.com>
2021-11-13 18:02:45 +0900

    [#21] 유튜브 키 local.properties로 분리

--- feature/music/data/src/main/java/com/mashup/kkyuni/feature/music/data/VideoService.kt
+++ feature/music/data/src/main/java/com/mashup/kkyuni/feature/music/data/VideoService.kt
@@ -2,6 +2,7 @@ package com.mashup.kkyuni.feature.music.data
 
 import com.mashup.kkyuni.feature.music.data.model.VideoDetailsResponse
 import com.mashup.kkyuni.feature.music.data.model.VideosResponse
+import com.mashup.kkyuni.music.data.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -11,13 +12,13 @@ interface VideoService {
         @Query("part") part: String = "snippet",
         @Query("q") query: String,
         @Query("type") type: String = "video",
-        @Query("key") key: String = "AIzaSyD_FgKW4mEpbNKMA09TrGhNd_QZhlqBK-M",
+        @Query("key") key: String = BuildConfig.YOUTUBE_API_KEY,
     ): VideosResponse
 
     @GET("videos")
     suspend fun getVideoInfo(
         @Query("part") part: String = "contentDetails",
         @Query("id") id: String,
-        @Query("key") key: String = "AIzaSyD_FgKW4mEpbNKMA09TrGhNd_QZhlqBK-M"
+        @Query("key") key: String = BuildConfig.YOUTUBE_API_KEY
     ): VideoDetailsResponse
 }
