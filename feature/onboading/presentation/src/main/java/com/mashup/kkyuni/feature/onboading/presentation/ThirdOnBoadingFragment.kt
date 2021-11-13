cae458dcb2ac94aac22364c6287344c1c6d7f21b
Huijiny <jiin20803@gmail.com>
2021-11-13 14:57:47 +0900

    [#58] 각 페이지 이미지 및 텍스트 삽입

--- feature/onboading/presentation/src/main/java/com/mashup/kkyuni/feature/onboading/presentation/ThirdOnBoadingFragment.kt
+++ feature/onboading/presentation/src/main/java/com/mashup/kkyuni/feature/onboading/presentation/ThirdOnBoadingFragment.kt
@@ -1,12 +1,6 @@
 package com.mashup.kkyuni.feature.onboading.presentation
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.mashup.kkyuni.core.BindingFragment
-import com.mashup.kkyuni.feature.onboading.presentation.databinding.FragmentOnboadingBinding
 import com.mashup.kkyuni.feature.onboading.presentation.databinding.FragmentThirdOnBoadingBinding
 
 class ThirdOnBoadingFragment : BindingFragment<FragmentThirdOnBoadingBinding>(
