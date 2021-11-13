1f73630138000a0057221fe3c012e1dd03a0091e
Huijiny <jiin20803@gmail.com>
2021-11-14 00:56:52 +0900

    [#58] 뷰페이저 현재 페이지 별 텍스트 변경

--- feature/onboading/presentation/src/main/java/com/mashup/kkyuni/feature/onboading/presentation/OnboadingFragment.kt
+++ feature/onboading/presentation/src/main/java/com/mashup/kkyuni/feature/onboading/presentation/OnboadingFragment.kt
@@ -2,6 +2,7 @@ package com.mashup.kkyuni.feature.onboading.presentation
 
 import android.os.Bundle
 import android.view.View
+import androidx.viewpager2.widget.ViewPager2
 import com.mashup.kkyuni.core.BindingFragment
 import com.mashup.kkyuni.feature.onboading.presentation.databinding.FragmentOnboadingBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -14,5 +15,33 @@ class OnboadingFragment : BindingFragment<FragmentOnboadingBinding>(
         super.onViewCreated(view, savedInstanceState)
 
         binding.viewPager.adapter = OnBoadingAdapter(this.requireActivity())
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == 0) {
+                    binding.prev.visibility = View.INVISIBLE
+                } else {
+                    binding.prev.visibility = View.VISIBLE
+                }
+
+                if (position == 2) {
+                    binding.next.text = "START!"
+                    binding.next.setTextColor(getResources().getColor(R.color.green_ddf880))
+                } else {
+                    binding.next.text = "NEXT"
+                    binding.next.setTextColor(getResources().getColor(R.color.white))
+                }
+            }
+        })
+        binding.next.setOnClickListener {
+            if (binding.viewPager.currentItem < 2) {
+                binding.viewPager.setCurrentItem(binding.viewPager.currentItem+1, true)
+            }
+        }
+        binding.prev.setOnClickListener {
+            if (binding.viewPager.currentItem > 0) {
+                binding.viewPager.setCurrentItem(binding.viewPager.currentItem-1, true)
+            }
+        }
     }
-}
+ }
